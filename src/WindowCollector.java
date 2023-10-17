2c7b3fb4c9d996cdc9ffba673046833a268ec94b
Scott Wegley <scbrwegley@protonmail.ch>
2023-10-17 07:53:29 -0700

    point collection class

--- src/WindowCollector.java
+++ src/WindowCollector.java
@@ -82,3 +82,27 @@ public class WindowCollector {
         return bi;
     }
 }
+
+class PointCollecter implements MouseListener {
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+}
