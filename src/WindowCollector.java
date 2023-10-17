bb72b0956c123ec5bc46d6f8dc971e8e7e3d8dc7
Scott Wegley <scbrwegley@protonmail.ch>
2023-10-17 07:47:31 -0700

    empty window collection

---
+++ src/WindowCollector.java
@@ -0,0 +1,18 @@
+package src;
+
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class WindowCollector {
+
+}
