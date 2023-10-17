010c26e82dc7562b706f53190fcc6bb439899d86
Scott Wegley <scbrwegley@protonmail.ch>
2023-10-17 07:44:59 -0700

    main app with swing imports

---
+++ src/MainApp.java
@@ -0,0 +1,12 @@
+package src;
+
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+class MainApp {
+
+}
