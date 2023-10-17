8cdb861ce5ebc9d57e31a469a9f74514c2c1953a
Scott Wegley <scbrwegley@protonmail.ch>
2023-10-17 07:46:29 -0700

    basic app, no integration

--- src/MainApp.java
+++ src/MainApp.java
@@ -8,5 +8,28 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 class MainApp {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Color Tiles Solver");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,300);
 
+        JPanel panel = new JPanel();
+
+        JLabel pLabel = new JLabel("Top Left: " + " | Bottom Right: ");
+        panel.add(pLabel);
+
+        JButton regionButton = new JButton("Select Game Region");
+        regionButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.setState(JFrame.ICONIFIED);
+            }
+            
+        });
+
+        frame.getContentPane().add(BorderLayout.SOUTH,panel);
+        frame.setVisible(true);
+
+    }
 }
